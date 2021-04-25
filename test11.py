18b2d790946f3d666b7b36e6d3f25195f7b657c9
Swasthikabhaskar <68891167+Swasthikabhaskar@users.noreply.github.com>
2021-04-25 19:21:23 +0530

    Create test11.py

---
+++ test11.py
@@ -0,0 +1,52 @@
+input_file =open("sample_input.txt","r")
+output_file = open("output.txt","w+")
+goodies={}
+output_list=[]
+for f in input_file:
+    index_toRead_price=f.index(":")
+    print(f[index_toRead_price+1:].strip())
+    print(f[:index_toRead_price])
+    goodies[f[:index_toRead_price]]=f[index_toRead_price+1:].strip()
+print(goodies)
+prices_only=list(goodies.values())
+prices_only=[int(i)for i in prices_only]
+prices_only.sort(reverse=True)
+print(prices_only)
+no_of_employees=int(input("Enter number of employees: "))
+length_considered=(len(prices_only)-no_of_employees)
+print(length_considered)
+for i in range(length_considered):
+    maxPrice=prices_only[i]
+    minPrice=prices_only[no_of_employees+i]
+    if i == 0:
+        difference=maxPrice-minPrice
+        choosen_index=i
+    elif (maxPrice-minPrice)<difference:
+        difference=maxPrice-minPrice
+        choosen_index=i
+
+choosen_prices=prices_only[choosen_index:no_of_employees+choosen_index]
+difference=max(choosen_prices)-min(choosen_prices)
+print("difference",difference)
+
+count=0
+for key,value in goodies.items():
+    prices_only[count]
+    print(value)
+    if int(value)in choosen_prices and count<no_of_employees:
+        str1=key+": "+value
+        output_list.append(str1)
+        count+=1
+        print(str1)
+output_file.write("goodies selected for distribution: ")
+output_file.write("/n")
+
+for i in output_list:
+    output_file.write(i)
+    output_file.write("\n")
+output_file.write("difference"+str(difference))
+input_file.close()
+output_file.close()
+        
+
+
